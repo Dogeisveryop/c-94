a44702eb2c57ac8c80008031ccc35ac0a29439f5
Doge <128231475+Dogeisveryop@users.noreply.github.com>
2023-06-20 18:02:45 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var  firebaseConfig = {
+    apiKey: "AIzaSyAI5uPhMdITjn6_eYJ9HfERivFPt6taYyE",
+    authDomain: "let-s-chat-7867c.firebaseapp.com",
+    databaseURL: "https://let-s-chat-7867c-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-7867c",
+    storageBucket: "let-s-chat-7867c.appspot.com",
+    messagingSenderId: "7537381363",
+    appId: "1:7537381363:web:fd370e6a088b333d384c86"
+  };
+  
+  // Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig);
